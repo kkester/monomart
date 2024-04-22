1af1c6f4f66aa8472a26465dd0b9795d2100b787
cnebs <charles.neblett@gmail.com>
2024-04-22 15:45:48 -0500

    Add Cypress Testing Library; add initial test.

--- frontend/cypress/e2e/spec.cy.ts
+++ frontend/cypress/e2e/spec.cy.ts
@@ -1,11 +1,12 @@
 describe("template spec", () => {
   it("passes", () => {
-    cy.visit("https://example.cypress.io");
+    cy.visit("localhost:8888");
   });
 });
 
 describe("View purchase history", () => {
   it("Shopper is on Home Page and navigates to the History", () => {
+    cy.visit("localhost:8888");
     cy.findByLabelText("History").click();
   })
 })
