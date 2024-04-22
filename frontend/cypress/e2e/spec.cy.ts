347950a5a97e9d421a1ba2d71533debc316277fd
cnebs <charles.neblett@gmail.com>
2024-04-22 16:34:22 -0500

    Enable cypress to see our tests

--- frontend/cypress/e2e/spec.cy.ts
+++ frontend/cypress/e2e/spec.cy.ts
@@ -1,12 +1,12 @@
 describe("template spec", () => {
   it("passes", () => {
-    cy.visit("localhost:8888");
+    cy.visit("http://localhost:8888");
   });
 });
 
 describe("View purchase history", () => {
   it("Shopper is on Home Page and navigates to the History", () => {
-    cy.visit("localhost:8888");
-    cy.findByLabelText("History").click();
+    cy.visit("http://localhost:8888");
+    cy.findByLabelText(/history/i).click();
   })
 })
